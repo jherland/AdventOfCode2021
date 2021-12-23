e64131b656069b4fd75ea71430d9fb6dcaa33a0e
Johan Herland <johan@herland.net>
2021-12-23 01:37:08 +0100

    Re-add Mypy to test script, as most files are testable now

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 set -e -x
 
-# mypy
+# Mypy does not yet handle all Python v3.10 features
+mypy 01.py 03.py 04.py 06.py 07.py 08.py 09.py 10.py 11.py 12.py 14.py 15.py 16.py 17.py 18.py 19.py 20.py 21.py 22.py
 black . --diff --color --check
 pflake8
